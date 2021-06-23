17decf7460de680868766eba18c0412e8302f278
EmilioAlposta <emilioalposta@gmail.com>
2021-06-22 23:10:33 -0300

    se agrego la estructura base del proyecto, asi como tambien las vistas basicas del mismo y el archivo app.js

---
+++ app.js
@@ -0,0 +1,26 @@
+const express = require('express')
+const app = express()
+const path = require('path');
+
+app.get('/', (req,res) =>{
+  res.sendFile(path.resolve(__dirname, './views/home.html'));
+});
+
+app.get('/Login', (req,res) =>{
+  res.sendFile(path.resolve(__dirname, './views/login.html'));
+});
+
+app.get('/Registro', (req,res) =>{
+  res.sendFile(path.resolve(__dirname, './views/registro.html'));
+});
+
+app.get('/Carrito', (req,res) =>{
+  res.sendFile(path.resolve(__dirname, './views/carrito-de-compras.html'));
+});
+
+app.use(express.static(path.resolve(__dirname, './public')));
+
+
+app.listen(3000, function() {
+  console.log("Corriendo servidor en el puerto 3000");
+});
