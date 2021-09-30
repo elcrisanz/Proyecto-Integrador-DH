924ef6f8ace684f53853178c2f1aae8032983907
Matisan2 <matisan2@hotmail.com>
2021-09-29 22:04:45 -0300

    Se agregan vistas de contacto y aboutUs

--- src/controllers/mainController.js
+++ src/controllers/mainController.js
@@ -10,7 +10,24 @@ const controlador = {
     },
     perfil: (req, res) => {
         res.render('./users/perfil');
-    }
+    },
+    aboutUs: (req, res) => {
+        res.render('./aboutUs');
+    },
+    contacto: (req, res) => {
+        res.render('./contacto');
+    },
+    message: (req, res) => {
+        let mensaje = {
+            name: req.body.name,
+            email: req.body.email,
+            telephone: req.body.telephone,
+            message: req.body.message
+        }
+        console.log(mensaje)
+        res.render('./index');
+    },
+
 };
 
 module.exports = controlador;
--- src/routes/mainRoutes.js
+++ src/routes/mainRoutes.js
@@ -6,5 +6,10 @@ const mainController = require('../controllers/mainController');
 router.get('/', mainController.index);
 router.get('/carrito', mainController.carrito);
 // router.get('/perfil', mainController.perfil)
+router.get('/aboutUs', mainController.aboutUs);
+router.get('/contacto', mainController.contacto);
+router.post('/contacto', mainController.message);
+
+
 
 module.exports = router;
